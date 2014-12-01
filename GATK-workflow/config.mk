e53a9b1d8a359feac9c854e011e5d1694730566e
Allen Day <aday@mapr.com>
2014-12-01 06:12:48 +0000

    refernce var

--- GATK-workflow/config.mk
+++ GATK-workflow/config.mk
@@ -1,8 +1,9 @@
-SAM_HEADER=./NA12878.new_header.sam
-ALIGN_BASE=./hg19.fa
-ANNOTATION=./vcf
-TOOLS=./
+SAM_HEADER=../NA12878.new_header.sam
+ALIGN_BASE=../hg19/hg19.fa
+ANNOTATION=../vcf
+TOOLS=../
+REFERENCE=../synthetic/synthetic.fa
 # 2 GB per thread is required for Data Threads
-DATA_THREADS=1
+DATA_THREADS=12
 # Memory is shared for CPU threads
-CPU_THREADS=1
+CPU_THREADS=12
