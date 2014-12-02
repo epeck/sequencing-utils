739350b79e4d1b5e074f24a02867172faf53f4e2
Allen Day <aday@mapr.com>
2014-12-02 21:03:33 +0000

    reduce stringency, increase verbosity

--- bin/picard_metrics.sh
+++ bin/picard_metrics.sh
@@ -5,23 +5,43 @@ OUTPUT="metrics/$PREFIX"
 
 for i in CollectGcBiasMetrics
 do
-  java -jar ${TOOLS}/picard/dist/picard.jar $i VERBOSITY=ERROR R=${ALIGN_BASE} I=$PREFIX.bam O=$OUTPUT.$i.out CHART=$OUTPUT.$i.pdf 2>$OUTPUT.$i.err
+  java -jar ${TOOLS}/picard/dist/picard.jar $i \
+    I=$PREFIX.bam \
+    R=${ALIGN_BASE} \
+    VALIDATION_STRINGENCY=LENIENT \
+    VERBOSITY=INFO \
+    O=$OUTPUT.$i.out \
+    CHART=$OUTPUT.$i.pdf \
+    2>$OUTPUT.$i.err
 done;
 
 for i in CollectOxoGMetrics CollectWgsMetrics
 do
-  java -jar ${TOOLS}/picard/dist/picard.jar $i VERBOSITY=ERROR R=${ALIGN_BASE} I=$PREFIX.bam O=$OUTPUT.$i.out 2>$OUTPUT.$i.err
+  java -jar ${TOOLS}/picard/dist/picard.jar $i \
+    I=$PREFIX.bam \
+    R=${ALIGN_BASE} \
+    VALIDATION_STRINGENCY=LENIENT \
+    VERBOSITY=INFO \
+    O=$OUTPUT.$i.out \
+    2>$OUTPUT.$i.err
 done;
 
 for i in CollectQualityYieldMetrics EstimateLibraryComplexity
 do
-  java -jar ${TOOLS}/picard/dist/picard.jar $i VERBOSITY=ERROR I=$PREFIX.bam O=$OUTPUT.$i.out 2>$OUTPUT.$i.err
+  java -jar ${TOOLS}/picard/dist/picard.jar $i \
+    I=$PREFIX.bam \
+    VALIDATION_STRINGENCY=LENIENT \
+    VERBOSITY=INFO \
+    O=$OUTPUT.$i.out \
+    2>$OUTPUT.$i.err
 done;
 
 java -jar ${TOOLS}/picard/dist/picard.jar CollectMultipleMetrics \
   I=$PREFIX.bam \
-  O=$OUTPUT. \
   R=${ALIGN_BASE} \
+  VALIDATION_STRINGENCY=LENIENT \
+  VERBOSITY=INFO \
+  O=$OUTPUT. \
   PROGRAM=null \
   PROGRAM=CollectAlignmentSummaryMetrics \
   PROGRAM=CollectInsertSizeMetrics \
