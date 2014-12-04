dae2084fa2351be23e392a6c6ac4957bb8363350
Allen Day <aday@mapr.com>
2014-12-04 22:15:16 +0000

    better loging

--- bin/picard_metrics.sh
+++ bin/picard_metrics.sh
@@ -5,6 +5,7 @@ OUTPUT="metrics/$PREFIX"
 
 for i in CollectGcBiasMetrics
 do
+  echo -n "$i...";
   java -jar ${TOOLS}/picard/dist/picard.jar $i \
     I=$PREFIX.bam \
     R=${ALIGN_BASE} \
@@ -13,10 +14,12 @@ do
     O=$OUTPUT.$i.out \
     CHART=$OUTPUT.$i.pdf \
     2>$OUTPUT.$i.err
+  echo "done";
 done;
 
 for i in CollectOxoGMetrics CollectWgsMetrics
 do
+  echo -n "$i...";
   java -jar ${TOOLS}/picard/dist/picard.jar $i \
     I=$PREFIX.bam \
     R=${ALIGN_BASE} \
@@ -24,31 +27,35 @@ do
     VERBOSITY=INFO \
     O=$OUTPUT.$i.out \
     2>$OUTPUT.$i.err
+  echo "done";
 done;
 
 for i in CollectQualityYieldMetrics EstimateLibraryComplexity
 do
+  echo -n "$i...";
   java -jar ${TOOLS}/picard/dist/picard.jar $i \
     I=$PREFIX.bam \
     VALIDATION_STRINGENCY=LENIENT \
     VERBOSITY=INFO \
     O=$OUTPUT.$i.out \
     2>$OUTPUT.$i.err
+  echo "done";
 done;
 
-java -jar ${TOOLS}/picard/dist/picard.jar CollectMultipleMetrics \
-  I=$PREFIX.bam \
-  R=${ALIGN_BASE} \
-  VALIDATION_STRINGENCY=LENIENT \
-  VERBOSITY=INFO \
-  O=$OUTPUT. \
-  PROGRAM=null \
-  PROGRAM=CollectAlignmentSummaryMetrics \
-  PROGRAM=CollectInsertSizeMetrics \
-  PROGRAM=QualityScoreDistribution \
-  PROGRAM=MeanQualityByCycle \
-  PROGRAM=CollectBaseDistributionByCycle \
-  2>$OUTPUT.CollectMultipleMetrics.err
+for i in CollectAlignmentSummaryMetrics CollectInsertSizeMetrics QualityScoreDistribution MeanQualityByCycle CollectBaseDistributionByCycle
+do
+  echo -n "CollectMultipleMetrics.$i...";
+  java -jar ${TOOLS}/picard/dist/picard.jar CollectMultipleMetrics \
+    I=$PREFIX.bam \
+    R=${ALIGN_BASE} \
+    VALIDATION_STRINGENCY=LENIENT \
+    VERBOSITY=INFO \
+    O=$OUTPUT. \
+    PROGRAM=null \
+    PROGRAM=$i \
+    2>$OUTPUT.CollectMultipleMetrics.$i.err
+  echo "done";
+done;
 
 #cannot  CalculateHsMetrics
 #cannot  CollectHiSeqXPfFailMetrics
